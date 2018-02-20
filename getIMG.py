28e618649858d69d6a1dd421438e0a4200edd2ac
404neme2018 <1031121634@qq.com>
2018-02-20 16:03:24 +0800

    finish getIMG.py

--- getIMG.py
+++ getIMG.py
@@ -17,5 +17,32 @@ def getName(index):
   name = name[0]
   return name
 
-print(getName(200))
-print(getName(1))
+def getImg(index, name_en, prepath='test'):
+  name_en = name_en.replace(' ', '_')
+  if not os.path.isdir(prepath):
+    os.makedirs(prepath)
+  for i in "ABCD":
+    #imgurl="http://img.fgowiki.com/fgo/card/servant/" +  '200' + i + '.png'
+    imgname = "{1:03d}{0}.{2}.png".format(i, index, name_en)
+    imgname = os.path.join(prepath, imgname)
+    imgurl="http://img.fgowiki.com/fgo/card/servant/{:03d}{}.png".format(index, i)
+    #print("Downloading {} from {}".format(imgname, imgurl))
+    urllib.request.urlretrieve(imgurl, imgname)
+    print("Downloaded {} from {}".format(imgname, imgurl))
+
+name_index = open("name_index.txt", 'w')
+for index in range(1, 201):
+  name = getName(index)
+  name_index.write("{:03d} {}\n".format(index, name))
+  ns = name.split(',')
+  name_en = ns[2]
+  getImg(index, name_en, prepath='img')
+#print(getName(200))
+#print(getName(1))
+#for name in nameRe:
+#    urllib.request.urlretrieve(imgurl,'{}{}.png'.format(name,x))
+#    x=x+1
+#  return name
+#html=gethtml("http://img.fgowiki.com/fgo/card/servant/200D.png)
+#print(getImg(html))
+
